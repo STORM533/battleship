2f9e46db0ad078ee84f5395814ee6e4ca08aba98
Saksham Kumar <146557098+STORM533@users.noreply.github.com>
2026-04-04 02:18:57 +0530

    gameboard done test cases created considering edge cases also .

--- src/tests/bs.spec.js
+++ src/tests/bs.spec.js
@@ -1,7 +1,62 @@
-import { Ship } from "../index.js";
+import { Ship,Gameboard } from "../index.js";
+//ship
 test('take a hit bih',()=>{
     expect(Ship(5).hit()).toBe(1);
 });
 test('titanic ahh',()=>{
     expect(Ship(5).isSunk()).toBe(false);
-})
+});
+//ganeboard
+test('places ship at given coordinates', () => {
+    const board = Gameboard();
+    const ship = Ship(2);
+
+    board.placeShip(ship, [[0,0],[0,1]]);
+
+    expect(board.allShipsSunk()).toBe(false);
+});
+test('attack hits ship', () => {
+    const board = Gameboard();
+    const ship = Ship(1);
+
+    board.placeShip(ship, [[0,0]]);
+    board.receiveAttack([0,0]);
+
+    expect(ship.isSunk()).toBe(true);
+});
+test('attack misses and is recorded', () => {
+    const board = Gameboard();
+    const ship = Ship(1);
+
+    board.placeShip(ship, [[0,0]]);
+    board.receiveAttack([5,5]);
+
+    expect(board.missedAttacks).toContainEqual([5,5]);
+});
+test('allShipsSunk returns true only when all ships are sunk', () => {
+    const board = Gameboard();
+
+    const ship1 = Ship(1);
+    const ship2 = Ship(1);
+
+    board.placeShip(ship1, [[0,0]]);
+    board.placeShip(ship2, [[1,1]]);
+
+    expect(board.allShipsSunk()).toBe(false);
+
+    board.receiveAttack([0,0]);
+    board.receiveAttack([1,1]);
+
+    expect(board.allShipsSunk()).toBe(true);
+});
+test('same coordinate attack does not double hit ship', () => {
+    const board = Gameboard();
+    const ship = Ship(2);
+
+    board.placeShip(ship, [[0,0],[0,1]]);
+
+    board.receiveAttack([0,0]);
+    board.receiveAttack([0,0]); // duplicate
+
+    expect(ship.isSunk()).toBe(false);
+});
