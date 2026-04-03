4a2ed53bd8f03bada8156f9e29154437d63da724
Saksham Kumar <146557098+STORM533@users.noreply.github.com>
2026-04-04 03:56:23 +0530

    Deployment commit

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./styles/styles.css";
-import {GameController} from './gameController';
-import { init } from './dom';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const game = GameController();
-    init(game);
-});
